a17c2a141272e4a78851b02338ae252f1af12d25
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-18 19:23:25 +0300

    Install dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 import express, { request } from 'express';
+import * as dotenv from 'dotenv';
 
-import {PORT} from './utils/config.js';
+
+dotenv.config();
 
 const app = express();
+const PORT_SERVER = process.env.PORT;
 
 app.get('/', (req, res) => {
-    console.log(req);
     return res.status(234).send('Welcome to Books shop')
     
 })
 
-app.listen(PORT, () => {
-    console.log(`App listening to port: ${PORT}`);
+app.listen(5555, () => {
+    console.log(`App listening to port: ${PORT_SERVER}`);
+    console.log(process.env.MONGODB_URL);
+    
     
 });
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://samolazovromanalex:<db_password>@cluster0.6tqir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
