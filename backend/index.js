54ba147957954e4762a2c2dea5cfd12791211d67
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-20 13:07:17 +0300

    Add route for get all books from DB

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,21 @@ app.post('/books', async (req, res) => {
         res.status(500).send({message:error.message});
     };
 });
+app.get('/books', async (req, res) => {
+    try {
+
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message:error.message});
+    }
+})
 
 app.listen(PORT_SERVER, () => {
 
