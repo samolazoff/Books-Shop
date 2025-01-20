0b09c43c62b93c4a26ff2abfae10b2de2d017b8a
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-20 19:40:15 +0300

    Add and setting react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
+import CreateBoook from './pages/CreateBook';
+import DeleteBoook from './pages/DeleteBook';
+import EditBoook from './pages/EditBook';
+import ShowBoook from './pages/ShowBook';
 
 
 const App = () => {
     return (
         <Routes>
-            <Route to='/' element={<Home></Home>}></Route>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/books/create' element={<CreateBoook></CreateBoook>}></Route>
+            <Route path='/books/details/:id' element={<ShowBoook></ShowBoook>}></Route>
+            <Route path='/books/edit/:id' element={<EditBoook></EditBoook>}></Route>
+            <Route path='/books/delete/:id' element={<DeleteBoook></DeleteBoook>}></Route>
         </Routes>
     )
 };
