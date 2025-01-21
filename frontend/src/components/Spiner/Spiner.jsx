bbdc8ef7143b92691a9b024017debb060be27602
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-21 19:14:53 +0300

    Create spiner

---
+++ frontend/src/components/Spiner/Spiner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spiner = () => {
+    return (
+        <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'>Spiner</div>
+    )
+};
+
+export default Spiner; 
