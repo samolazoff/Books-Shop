1f85c8b6059f32fe5cb3a60309c51a7bc3fa57a9
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-23 15:24:26 +0300

    Create page show book

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+
+import BackButton from '../components/BackButton/BackButton';
+import Spiner from '../components/Spiner/Spiner';
 
 const ShowBook = () => {
-  return (
-    <div>ShowBook</div>
-  )
-}
+    const [book, setBook] = useState({});
+    const [loading, setLoading] = useState(false);
+    const {id} = useParams();
 
-export default ShowBook
+    useEffect(
+        () => {
+            setLoading(true);
+            axios
+                .get(`http://localhost:5555/books/${id}`)
+                .then((res) => {
+                    setBook(res.data);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setLoading(false);
+                })
+        }, []
+    );
+    return (
+        <div className='p-4'>
+            <BackButton></BackButton>
+            <h1 className='text-3xl my-4'>Show Book</h1>
+            {
+                loading ? (<Spiner></Spiner>):(
+                    <div className="flex flex-col border-2 border-sky-400 rounded-xl">
+                        <div className="my-4">
+                            <span className="text-xl mr-4 text-gray-500">Title</span>
+                            <span>{book.title}</span>
+                        </div>
+                        <div className="my-4">
+                            <span className="text-xl mr-4 text-gray-500">Author</span>
+                            <span>{book.author}</span>
+                        </div>
+                        <div className="my-4">
+                            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+                            <span>{book.publishYear}</span>
+                        </div>
+                        <div className="my-4">
+                            <span className="text-xl mr-4 text-gray-500">Create time</span>
+                            <span>{new Date(book.createdAt).toString()}</span>
+                        </div>
+                        <div className="my-4">
+                            <span className="text-xl mr-4 text-gray-500">Last update time</span>
+                            <span>{new Date(book.updatedAt).toString()}</span>
+                        </div>
+                        
+                    </div>
+                )
+            }
+        </div>
+    )
+};
+
+export default ShowBook;
